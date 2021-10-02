b8b56c3c20fda7238ae56bbd061a3a645e66172d
Doug McKay <18455931+damky@users.noreply.github.com>
2021-10-02 10:39:09 -0600

    turns it off

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import BlockContent from "@sanity/block-content-to-react";
+// import BlockContent from "@sanity/block-content-to-react";
 // import imageUrlBuilder from "@sanity/image-url";
 import client from "./utils/useSanity";
 import { /* Slide, P, */ RevealJS } from "@gregcello/revealjs-react";
